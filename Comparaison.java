b4bb9cd42af93e4d1282167fbbfdf90b568307b9
RegisteDjessou <41454186+RegisteDjessou@users.noreply.github.com>
2021-01-12 20:58:28 +0100

    Update Comparaison.java

--- Comparaison.java
+++ Comparaison.java
@@ -12,6 +12,8 @@ public class Comparaison {
 		
 		System.out.println("args avant tri: "+tab);
 		System.out.println("tri lexico:"+objet.triLexico(args));
+		System.out.print("tri militaire:");objet.triMilitaire(args);
+		System.out.println("tri Inverse lexico:"+objet.triInverseLexico(args));
 		
 
 	}
@@ -26,15 +28,36 @@ public class Comparaison {
 		return tab1;
 	}
 	
+	public void triMilitaire(String[] tab) {
+		
+		ArrayList<String> tab1= new ArrayList<String>();
+		Map<Integer, String> m = new HashMap<Integer, String>();
+		for(String chaine :tab) {tab1.add(chaine);}
+		Collections.sort(tab1);
+		for (Iterator<String> i = tab1.iterator();i.hasNext(); ){
+			
+			String o = i.next();
+			m.put(o.length(), o);
+			List<Integer> employeeByKey = new ArrayList<Integer>(m.keySet());
+			Collections.sort(employeeByKey);
+			
+			
+		}
+		System.out.println(m.values());
+		
+	}
 	
-	
+	public ArrayList<String> triInverseLexico(String[] tab) {
+			
+			ArrayList<String> tab1= new ArrayList<String>();
+			for(String chaine :tab) {tab1.add(chaine);}
+			Collections.sort(tab1);
+			Collections.reverse(tab1);
+			
+			return tab1;
+		}
 	
 
-	public int countMot(String mot) {return mot.length();}
-
-	
-	
-	
 	
 	
 
